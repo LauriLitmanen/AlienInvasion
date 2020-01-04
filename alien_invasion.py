ba2b50461b680b98065e5bd4348550ba82baa7fb
LauriLitmanen <litmanen.lauri@gmail.com>
2020-01-04 14:32:39 +0200

    [alien fleet]create a fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -96,7 +96,16 @@ class AlienInvasion:
 		"""Create the fleet of aliens """
 		# Make an alien.
 		alien = Alien(self)
-		self.aliens.add(alien) 
+		alien_width = alien.rect.width
+		available_space_x = self.settings.screen_width - (2 * alien_width)
+		number_aliens_x = available_space_x // (2 * alien_width)
+
+		# Create the first row of Aliens
+		for alien_number in range(number_aliens_x):
+			alien = Alien(self)
+			alien.x = alien_width + 2 * alien_width * alien_number
+			alien.rect.x = alien.x
+			self.aliens.add(alien)
 	
 	def _update_screen(self):
 		"""Update images on the screen and flip to the new image """
